af6992eefbb7bf5f63d5b022837cfa6950be3045
Geoffroy Lambert <geoffroy.lambert042@outlook.com>
2025-03-23 10:46:23 -0400

    title screen scene & tutorial

--- Doodlefeels33/Assets/main_menu.cs
+++ Doodlefeels33/Assets/main_menu.cs
@@ -3,6 +3,24 @@ using UnityEngine.SceneManagement;
 
 public class main_menu : MonoBehaviour
 {
+    [SerializeField]
+    GameObject title_screen, black_screen, tutorial1_screen, tutorial2_screen;
+
+    public void StartTutorial()
+    {
+        title_screen.SetActive(false);
+        black_screen.SetActive(true);
+    }
+
+    public void NextStep()
+    {
+        tutorial1_screen.SetActive(false);
+        tutorial2_screen.SetActive(true);
+    }
+
+
+
+
     public void NewGame()
     {
         SceneManager.LoadScene("MainGame");
