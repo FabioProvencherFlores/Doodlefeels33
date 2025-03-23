981058e4261e0e3b5d611bd9e81c6a9322c2944f
Fabio Provencher-Flores <57635847+FabioProvencherFlores@users.noreply.github.com>
2025-03-23 10:35:49 -0400

    it and dialogue skip

--- Doodlefeels33/Assets/scripts/NPCs/itNPC.cs
+++ Doodlefeels33/Assets/scripts/NPCs/itNPC.cs
@@ -16,31 +16,79 @@ public class itNPC : BeigeNPC, IDialogue
 
 	public string GetNextDialogueString()
 	{
-		return "";
-	}
+		removeGoodbye = false;
+		dialogueOptions.Clear();
+		SITUATION previousSituation = currentContext;
+		currentContext = nextContext;
+		string currentline = "I SHOULD NOT SAY THIS, FAB MUST HAVE FORGOTTEN SOMETHING";
 
-	public void InitNewDialogue()
-	{
 
+		switch (currentContext)
+		{
+			case SITUATION.NormalGreating:
+				currentline = "I heard noise over the radio but the battery is dead. Maybe I can some juice out of this. The transistor is busted... Ho, can I help you?";
+				break;
+			case SITUATION.PlayerAskedToGoToJail:
+				removeGoodbye = true;
+				currentline = "I won't be of any use in total darkness. Think about it, please.";
+				dialogueOptions.Add("You are right. Stay with us.");
+				dialogueOptions.Add("You need to go inside, it won't be for long.");
+				break;
+			case SITUATION.BackedDownFromJailRequest:
+				currentline = "Need anything else?";
+				break;
+		}
+
+
+		return currentline;
 	}
 
 	public void ProcessDialogueOption(int optionID)
 	{
+		switch (currentContext)
+		{
+			case SITUATION.PlayerAskedToGoToJail:
+				myData.playerHasAskedForJail = true;
+				if (optionID == 0) nextContext = SITUATION.BackedDownFromJailRequest;
+				if (optionID == 1)
+				{
+					GameManager.Instance.PutCurrentNPCInJail();
+					GameManager.Instance.GoToGym();
+					return;
+				}
+				goto case SITUATION.PassiveChecks;
+			case SITUATION.NormalGreating:
+			case SITUATION.BackedDownFromJailRequest:
+			case SITUATION.PassiveChecks:
+			default:
+				if (optionID == 3)
+				{
+					GameManager.Instance.GoToGym();
+				}
+				break;
+		}
 
+		// 4 is jail
+		if (optionID == 4)
+		{
+			nextContext = SITUATION.PlayerAskedToGoToJail;
+			myData.playerWantsToJailMe = true;
+		}
 	}
 
 	public SITUATION GetInitialContext()
 	{
-		return SITUATION.INVALID;
+		return SITUATION.NormalGreating;
 	}
 
 	public int GetNPCID()
 	{
-		return -1;
+		return 3;
 	}
-
-	public bool IsGoodbyeADefaultOption()
+	public void InitNewDialogue()
 	{
-		return true;
+		currentContext = GetInitialContext();
+		nextContext = currentContext;
+		myData.Reset();
 	}
 }
